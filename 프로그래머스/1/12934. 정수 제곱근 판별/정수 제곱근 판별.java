259e0b22aceeb5bb93bdb6553be2f692f5bd2aa2
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-10 09:58:32 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        if(sqrt == (long)sqrt){
+            return (long) Math.pow(sqrt+1,2);
+        }
+        return -1;
+    }
+}
