0ba0547acd3505ed63f22b250169fa02dfd6bb42
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-16 09:29:19 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,9 +2,10 @@ class Solution {
     public String solution(String[] seoul) {
         for(int i = 0; i < seoul.length; i++){
             if("Kim".equals(seoul[i])){
-             return "김서방은 " + i + "에 있다";
+            String location = integer.toString(i);
+            return location;
             }
         }
-       return "";
+        return "김서방은" + location + "에 있다";
     }
 }
