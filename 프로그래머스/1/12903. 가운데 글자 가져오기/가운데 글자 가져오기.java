e7a599d6adfaca0ea16083377ed66a802e3b6c32
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-01-25 23:40:52 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int length = s.length();
+        int mid = length / 2 ;
+        
+        return (length % 2 == 0) 
+            ? s.substring(mid - 1, mid + 1)
+            : s.substring(mid, mid + 1);
+                
+    }
+}
