15c788e2215317e1f78f317af521d6a43d3800b6
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-18 09:44:25 +0900

    [level 1] Title: 과일 장수, Time: 50.77 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,14 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         Arrays.sort(score);
-        int n = score.length;
-        int maxProfit = 0;
+        int answer = 0;
 
-        for (int i = n - m; i >= 0; i -= m) {
-            int p = score[i];
-            maxProfit += p * m;
+        for (int i = score.length; i >= m; i -= m) {
+            answer += score[i - m] * m;
         }
         
-        return maxProfit;
+        return answer;
     }
 }
